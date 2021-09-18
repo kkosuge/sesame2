61248486f6dc437f547e381c52f09c184f7aa9a7
kkosuge <root@kksg.net>
2021-09-18 21:29:59 +0900

    delete unnecessary test

--- spec/sesame2_spec.rb
+++ spec/sesame2_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Sesame2 do
   it "has a version number" do
     expect(Sesame2::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
